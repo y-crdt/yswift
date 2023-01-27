4dd8ed81a41428a3babdc5574031a89ed82ee7b5
Joe Heck <heckj@mac.com>
2023-01-27 11:36:01 -0800

    reset XCFramework using xcodebuild
    - WIP, commented out earlier code
    - added macOS/Darwin to the sequence
    - unclear if we need a unified header structure
    - pulled in generated Modulemap instead of replicated copy in pkg
    - most of `pkg` is rather redundant now (I think)

--- lib/build-xcframework.sh
+++ lib/build-xcframework.sh
@@ -21,8 +21,10 @@ rustup target add aarch64-apple-ios # iOS Device
 rustup target add aarch64-apple-darwin # macOS platform (M1)
 rustup target add x86_64-apple-darwin # macOS platform (x86)
 
-echo "▸ Install uniffi bindgen"
-cargo install uniffi_bindgen
+if [ ! -x $HOME/.cargo/bin/uniffi-bindgen ]; then
+    echo "▸ Install uniffi bindgen"
+    cargo install uniffi_bindgen
+fi
 
 # (heckj - 26jan2023): remove the following line IF we start to include/check in
 # the local Cargo.lock file. The `--locked` command line options on the
@@ -38,7 +40,7 @@ mkdir -p "${SWIFT_FOLDER}/scaffold"
 echo "▸ Generate Swift Scaffolding Code"
 $HOME/.cargo/bin/uniffi-bindgen generate "src/yniffi.udl" --language swift --out-dir "${SWIFT_FOLDER}/scaffold"
 # Remove unused generated .modulemap
-rm "${SWIFT_FOLDER}/scaffold/${PACKAGE_NAME}FFI.modulemap"
+#rm "${SWIFT_FOLDER}/scaffold/${PACKAGE_NAME}FFI.modulemap"
 
 echo "▸ Building for x86_64-apple-ios"
 CFLAGS_x86_64_apple_ios="-target x86_64-apple-ios" \
@@ -60,45 +62,57 @@ echo "▸ Building for x86_64-apple-darwin"
 CFLAGS_x86_64_apple_ios="-target x86_64-apple-darwin" \
 cargo build --target x86_64-apple-darwin --package "${PACKAGE_NAME}" --locked --release
 
-# (heckj-26jan2023): A lot of this is based on a script I created, and which I in turn used
-# code from https://github.com/automerge/automerge-swift-backend/blob/main/cargo_xcframeworks.sh
-# The process of creating the XCFramework _can_ be done with just copying bits in the right
-# places and names, but more generally uses `xcodebuild -create-xcframework` to have Xcode
-# do the "heavy lifting". I'm unclear if there was a reason we wanted (or needed) to avoid
-# a dependency on Xcode here...
-#
 # Upstream documentation on creating XCFramework bundles:
 # https://help.apple.com/xcode/mac/11.4/#/dev544efab96
 # https://developer.apple.com/documentation/xcode/creating-a-multi-platform-binary-framework-bundle
 # https://developer.apple.com/documentation/xcode/distributing-binary-frameworks-as-swift-packages
 # WWDC 2019 video on generating binaries: https://developer.apple.com/videos/play/wwdc2019/416/
 
-echo "▸ Starting xcframework creation"
-echo "▸ Copy necessary files"
-mkdir -p "${XCFRAMEWORK_FOLDER}/ios-arm64/${FRAMEWORK_FOLDER}"
-cp "$SWIFT_FOLDER/pkg/Info.plist" "$XCFRAMEWORK_FOLDER"
-cp -r "${SWIFT_FOLDER}/pkg/Headers" "${XCFRAMEWORK_FOLDER}/ios-arm64/${FRAMEWORK_FOLDER}"
-cp -r "${SWIFT_FOLDER}/pkg/Modules" "${XCFRAMEWORK_FOLDER}/ios-arm64/${FRAMEWORK_FOLDER}"
-mv "${SWIFT_FOLDER}/scaffold/${PACKAGE_NAME}FFI.h" "${XCFRAMEWORK_FOLDER}/ios-arm64/${FRAMEWORK_FOLDER}/Headers"
-# Duplicate same layout for both architectures
-cp -r "${XCFRAMEWORK_FOLDER}/ios-arm64" "${XCFRAMEWORK_FOLDER}/ios-arm64_x86_64-simulator"
+# echo "▸ Copy necessary files"
+# mkdir -p "${XCFRAMEWORK_FOLDER}/ios-arm64/${FRAMEWORK_FOLDER}"
+# cp "$SWIFT_FOLDER/pkg/Info.plist" "$XCFRAMEWORK_FOLDER"
+# cp -r "${SWIFT_FOLDER}/pkg/Headers" "${XCFRAMEWORK_FOLDER}/ios-arm64/${FRAMEWORK_FOLDER}"
+# cp -r "${SWIFT_FOLDER}/pkg/Modules" "${XCFRAMEWORK_FOLDER}/ios-arm64/${FRAMEWORK_FOLDER}"
+# mv "${SWIFT_FOLDER}/scaffold/${PACKAGE_NAME}FFI.h" "${XCFRAMEWORK_FOLDER}/ios-arm64/${FRAMEWORK_FOLDER}/Headers"
+# # Duplicate same layout for both architectures
+# cp -r "${XCFRAMEWORK_FOLDER}/ios-arm64" "${XCFRAMEWORK_FOLDER}/ios-arm64_x86_64-simulator"
 
-echo "▸ Lipo & move iOS Simulator static library to xcframework"
+echo "▸ Create a unified header pool for the XCFramework"
+# Creating this pool, with the unified header, with the suspicion that we might want to use
+# more than a single rust-based library in our combined swift package.
+mkdir -p "${BUILD_FOLDER}/FFI_headers"
+cp -r "${SWIFT_FOLDER}/pkg/Headers/" "${BUILD_FOLDER}/FFI_headers"
+cp "${SWIFT_FOLDER}/scaffold/yniffiFFI.h" "${BUILD_FOLDER}/FFI_headers"
+cp "${SWIFT_FOLDER}/scaffold/yniffiFFI.modulemap" "${BUILD_FOLDER}/FFI_headers"
+
+echo "▸ Lipo (merge platforms) iOS Simulator static library for consuming into an XCFramework"
+mkdir -p "./${BUILD_FOLDER}/apple-ios-simulator/release"
 lipo -create  \
     "./${BUILD_FOLDER}/x86_64-apple-ios/release/${LIB_NAME}" \
     "./${BUILD_FOLDER}/aarch64-apple-ios-sim/release/${LIB_NAME}" \
-    -output "$XCFRAMEWORK_FOLDER/ios-arm64_x86_64-simulator/$FRAMEWORK_FOLDER/$FRAMEWORK_NAME"
+    -output "./${BUILD_FOLDER}/apple-ios-simulator/release/${LIB_NAME}"
 
-echo "▸ Lipo & move macOS static library to xcframework"
+echo "▸ Lipo (merge platforms) macOS static library for consuming into an XCFramework"
+mkdir -p "./${BUILD_FOLDER}/apple-darwin/release"
 lipo -create  \
     "./${BUILD_FOLDER}/x86_64-apple-darwin/release/${LIB_NAME}" \
     "./${BUILD_FOLDER}/aarch64-apple-darwin/release/${LIB_NAME}" \
-    -output "$XCFRAMEWORK_FOLDER/apple-darwin/$FRAMEWORK_FOLDER/$FRAMEWORK_NAME"
+    -output "./${BUILD_FOLDER}/apple-darwin/release/${LIB_NAME}"
 
-echo "▸ Move iOS Device static library to xcframework"
-cp "$BUILD_FOLDER/aarch64-apple-ios/release/$LIB_NAME" "$XCFRAMEWORK_FOLDER/ios-arm64/$FRAMEWORK_FOLDER/$FRAMEWORK_NAME"
+# echo "▸ Move iOS Device static library to xcframework"
+# cp "$BUILD_FOLDER/aarch64-apple-ios/release/$LIB_NAME" "$XCFRAMEWORK_FOLDER/ios-arm64/$FRAMEWORK_FOLDER/$FRAMEWORK_NAME"
 
-echo "▸ Compress xcframework"
+echo "▸ Create ${XCFRAMEWORK_FOLDER}"
+  xcodebuild -create-xcframework \
+            -library "./$BUILD_FOLDER/apple-ios-simulator/release/${LIB_NAME}" \
+            -headers "./${BUILD_FOLDER}/FFI_headers" \
+            -library "./$BUILD_FOLDER/aarch64-apple-ios/release/${LIB_NAME}" \
+            -headers "./${BUILD_FOLDER}/FFI_headers" \
+            -library "./$BUILD_FOLDER/apple-darwin/release/${LIB_NAME}" \
+            -headers "./${BUILD_FOLDER}/FFI_headers" \
+            -output "${XCFRAMEWORK_FOLDER}"
+
+echo "▸ Compress XCFramework"
 ditto -c -k --sequesterRsrc --keepParent "$XCFRAMEWORK_FOLDER" "$XCFRAMEWORK_FOLDER.zip"
 
 echo "▸ Compute checksum"
