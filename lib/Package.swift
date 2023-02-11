d9e6f6c122be016968f4fabaa14cf4f0c461174c
Joe Heck <heckj@mac.com>
2023-02-11 13:19:04 -0800

    XCFramework build update
    - replaced hand-assembly directory with xcodebuild -create-framework
    - added macOS support for Cargo and target generation, lipo'd static
      binary
    - added explicit macOS support in Package.swift (10.15)
    - adding generated swift, and related C header, into source control
    
    verified results in Xcode directly, at least with this package - follow
    on updates would need to apply to YSwift to add macOS support there as
    well

--- lib/Package.swift
+++ lib/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Yniffi",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(name: "Yniffi", targets: ["Yniffi"]),
     ],
