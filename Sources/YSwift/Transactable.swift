12248748d7aa8039996dc1683e4234a6360f1901
Joe Heck <heckj@mac.com>
2023-04-29 16:13:27 -0700

    transactable docs

--- Sources/YSwift/Transactable.swift
+++ Sources/YSwift/Transactable.swift
@@ -14,6 +14,12 @@ protocol Transactable {
 }
 
 extension Transactable {
+    /// A convenience accessor to interacting with the shared data types within a document.
+    ///
+    /// - Parameters:
+    ///   - transaction: An optional transaction that, if provided, is passed to the trailing closure. If not provided, a new transaction is requested from the ``YDocument``.
+    ///   - changes: A trailing closure that provides the transaction you use to interact with shared types.
+    /// - Returns: Returns the returned value from the trailing closure.
     func withTransaction<T>(_ transaction: YrsTransaction? = nil, changes: @escaping (YrsTransaction) -> T) -> T {
         if let transaction = transaction {
             return changes(transaction)
