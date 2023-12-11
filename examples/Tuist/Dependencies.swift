f9a28dbf8c39a05e8002bba20eba62e6e03e19fd
Joe Heck <heckj@mac.com>
2023-12-11 10:24:40 -0800

    updating tuist dependency configuration based on example at https://github.com/Saik0s/Whisperboard/blob/main/Tuist/Dependencies.swift

--- examples/Tuist/Dependencies.swift
+++ examples/Tuist/Dependencies.swift
@@ -1,8 +1,12 @@
 import ProjectDescription
 
+let packages: [Package] = [
+    .package(url: "https://github.com/y-crdt/yswift", from: "0.1.0"),
+]
+
 let dependencies = Dependencies(
-    swiftPackageManager: [
-        .remote(url: "https://github.com/y-crdt/yswift", requirement: .upToNextMajor(from: "0.1.0")),
-    ],
+    swiftPackageManager: SwiftPackageManagerDependencies(
+        packages
+    ),
     platforms: [.iOS]
 )
