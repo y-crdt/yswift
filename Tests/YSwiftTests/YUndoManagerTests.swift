8ec2e2821d19151ea656a3fbb588f190baa7a928
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2024-03-03 17:35:58 +0100

    undo manager test: use origin to track source of changes

--- Tests/YSwiftTests/YUndoManagerTests.swift
+++ Tests/YSwiftTests/YUndoManagerTests.swift
@@ -9,6 +9,27 @@ class TestMetadata {
     }
 }
 
+func exchangeUpdates(_ document1: YDocument, _ document2: YDocument) {
+    let clock1 = document1.transactSync { txn in
+        txn.transactionStateVector()
+    }
+    let clock2 = document2.transactSync { txn in
+        txn.transactionStateVector()
+    }
+    let update1 = document1.transactSync { txn in
+        document1.diff(txn: txn, from: clock2)
+    }
+    let update2 = document2.transactSync { txn in
+        document2.diff(txn: txn, from: clock1)
+    }
+    document1.transactSync { txn in
+        try! txn.transactionApplyUpdate(update: update2)
+    }
+    document2.transactSync { txn in
+        try! txn.transactionApplyUpdate(update: update1)
+    }
+}
+
 class YUndoManagerTests: XCTestCase {
     var document: YDocument!
     var text: YText!
@@ -65,5 +86,45 @@ class YUndoManagerTests: XCTestCase {
         
         XCTAssert (try manager.redo())
         XCTAssertEqual(received.value, "A")
+    }
+    
+    func test_undoDifferentOrigins() throws {
+        let localOrigin = Origin("local")
+        
+        let remoteDocument = YDocument()
+        let remoteText = document.getOrCreateText(named: "test")
+        manager.addOrigin(localOrigin) // only track transaction from local origin
+        
+        // create some changes locally
+        document.transactSync(origin: localOrigin) { txn in
+            self.text.insert("hello", at: 0)
+            self.manager.wrap() // add changes on a stack: they will be undone as one
+        }
+        
+        exchangeUpdates(document, remoteDocument)
+        
+        // concurrent change on the remote replica
+        document.transactSync(origin: localOrigin) { txn in
+            self.text.insert(" world", at: 5)
+        }
+        remoteText.insert("<break>", at: 1)
+        
+        XCTAssertEqual(text.getString(), "hello world")
+        
+        exchangeUpdates(document, remoteDocument)
+        
+        XCTAssertEqual(text.getString(), "h<break>ello world")
+        XCTAssertEqual(remoteText.getString(), "h<break>ello world")
+        
+        XCTAssertTrue (try manager.undo())
+        
+        // only changes marked locally have been reversed
+        XCTAssertEqual(text.getString(), "h<break>ello")
+        
+        XCTAssertTrue (try manager.undo())
+        XCTAssertEqual(text.getString(), "<break>")
+        
+        XCTAssertFalse(try manager.undo()) // remote changes are not reverted
+        XCTAssertEqual(text.getString(), "<break>")
     }
 }
