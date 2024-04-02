a7475a80789684eac2adaf8fa5724b546995a845
Fernando Barbat <fbarbat@gmail.com>
2024-04-02 15:42:34 -0300

    removed memory stress test

--- Tests/YSwiftTests/YSubscriptionTests.swift
+++ Tests/YSwiftTests/YSubscriptionTests.swift
@@ -53,40 +53,4 @@ class YSubscriptionTests: XCTestCase {
             subscription?.cancel()
         }
     }
-    
-    // This causes memory to keep growing and I am not sure why
-    // I checked that UniFFI YSubscription struct is being dropped accordingly
-    // I also verified that the memory increase also happened when we used subscriptionIds and unobserve
-    func disabled_test_allocateTooMany() {
-        for i in 1...1000000000 {
-            _ = text.observe { _ in }
-            
-            if i % 1000000 == 0 {
-                print("\(Date()) Iterations: \(i) Resident Size: \(residentSize() ?? 0)")
-            }
-        }
-        
-        print("Done!")
-    }
-    
-    private func residentSize() -> UInt64? {
-        var info = mach_task_basic_info()
-        let MACH_TASK_BASIC_INFO_COUNT = MemoryLayout<mach_task_basic_info>.stride/MemoryLayout<natural_t>.stride
-        var count = mach_msg_type_number_t(MACH_TASK_BASIC_INFO_COUNT)
-
-        let kerr: kern_return_t = withUnsafeMutablePointer(to: &info) {
-            $0.withMemoryRebound(to: integer_t.self, capacity: MACH_TASK_BASIC_INFO_COUNT) {
-                task_info(mach_task_self_,
-                          task_flavor_t(MACH_TASK_BASIC_INFO),
-                          $0,
-                          &count)
-            }
-        }
-
-        guard kerr == KERN_SUCCESS else {
-            return nil
-        }
-        
-        return info.resident_size
-    }
 }
